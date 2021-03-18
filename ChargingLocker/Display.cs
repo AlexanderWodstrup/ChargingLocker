1dfaead3188b78500ca3dd52c51fe3e919f67132
pallemanden <58727279+pallemanden@users.noreply.github.com>
2021-03-18 14:01:43 +0100

    Display class added

---
+++ ChargingLocker/Display.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargingLocker
+{
+    class Display
+    {
+        public void DisplayConnectPhone()
+        {
+            Console.WriteLine("Please connect your phone");
+        }
+
+        public void DisplayScanRFID()
+        {
+            Console.WriteLine("Please scan your tag");
+        }
+
+        public void DisplayFailedConnection()
+        {
+            Console.WriteLine("The system could not connect to your device");
+        }
+
+        public void DisplayChargeLockerOccupied()
+        {
+            Console.WriteLine("This locker is occupied by a user");
+        }
+
+        public void DisplayWrongRFID()
+        {
+            Console.WriteLine("Your tag could not be found, please try again");
+        }
+
+        public void DisplayRemovePhone()
+        {
+            Console.WriteLine("Please remove your device");
+        }
+    }
+}
