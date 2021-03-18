21d9890a0b0686c9ecd9dee4d183a6a993c9a05b
AlexanderWodstrup <Alexander.f.Wodstrup@gmail.com>
2021-03-18 13:52:56 +0100

    Door.cs done

---
+++ ChargingLocker/Door.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargingLocker
+{
+    class Door
+    {
+        private bool _doorOpen = false;
+        private bool _doorClose = true;
+        private bool _lock = false;
+
+        void DoorOpened()
+        {
+            _doorClose = false;
+            _doorOpen = true;
+        }
+
+        void DoorClosed()
+        {
+            _doorOpen = false;
+            _doorClose = true;
+        }
+
+        void LockDoor()
+        {
+            _lock = true;
+        }
+
+        void UnlockDoor()
+        {
+            _lock = false;
+        }
+    }
+}
