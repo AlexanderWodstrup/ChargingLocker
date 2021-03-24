7f4cda1b3a5aa993afa3c88a2a8185e6d3a1ce94
AlexanderWodstrup <Alexander.f.Wodstrup@gmail.com>
2021-03-24 20:05:15 +0100

    Fordelt filer i forskellige projekter

--- ChargingLocker.ClassLibrary/LogWriter.cs
+++ ChargingLocker.ClassLibrary/LogWriter.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ChargingLocker;
 
-using UsbSimulator;
-
-namespace ChargingLocker
+namespace ChargingLocker.ClassLibrary
 {
     public class LogWriter
     {
--- ChargingLocker.ClassLibrary/RFIDReaderSimulator.cs
+++ ChargingLocker.ClassLibrary/RFIDReaderSimulator.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ChargingLocker;
 
-namespace RFIDSimulator
+namespace ChargingLocker.ClassLibrary
 {
     class RFIDReaderSimulator : IRFIDReader
     {
